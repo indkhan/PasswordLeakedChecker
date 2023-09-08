d17a20ec8036e2cf2fde92ee7776db6c43ab667c
Usman Khan <mgsukhan@gmail.com>
2023-09-09 00:53:19 +0300

    tells no of time

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import requests
 import hashlib
 
-text = "1234565545"
+text = "12345678"
 password = hashlib.sha1(text.encode('utf-8')).hexdigest().upper()
 print(password)
 
 leak = requests.get(f"https://api.pwnedpasswords.com/range/{password[:5]}")
-print(leak.text)
+numandpass = leak.text
+lines = numandpass.splitlines()
+print(numandpass)
+
+
+for line in lines: 
+    che = line.split(":")
+    if password[5:] == che[0]:
+        print(text , 'is there ',che[1],"times")
+
+
